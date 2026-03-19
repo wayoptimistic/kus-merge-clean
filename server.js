44622bd7857f3be6b903d1e2b8d23a8eb873e924
wayoptimistic <jbehrens57@gmail.com>
2026-03-19 13:33:33 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const s3 = new S3Client({
   region: "auto",
   endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
+    forcePathStyle: true
     accessKeyId: process.env.R2_ACCESS_KEY,
     secretAccessKey: process.env.R2_SECRET_KEY,
   },
