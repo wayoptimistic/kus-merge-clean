7b0db1f408e1c4a27ddf6bc1130111addcc4bb4b
wayoptimistic <jbehrens57@gmail.com>
2026-03-19 13:36:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const s3 = new S3Client({
   region: "auto",
   endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    forcePathStyle: true
     accessKeyId: process.env.R2_ACCESS_KEY,
     secretAccessKey: process.env.R2_SECRET_KEY,
   },
+  forcePathStyle: true
 });
-
 // ==============================
 // ROOT (health check)
 // ==============================
